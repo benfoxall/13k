e64ac92cc3a09e6e56e7ccf2d9ebb4250c7dbdf0
Ben Foxall <ben.foxall@oxbotica.com>
2019-08-17 00:49:58 +0100

    Does a link make things more robust?

--- hacks/script.js
+++ hacks/script.js
@@ -1,21 +1,37 @@
 const n = 20;
 
-if(!window.history.state) {
-  for(let i = 0; i < n; i++) {
-    history.pushState(i, "num" + i, "?h-" + i)
+if (!window.history.state) {
+  const a = document.createElement('a')
+  a.textContent = 'start'
+  a.href = "#"
+  a.onclick = () => {
+
+    for (let i = 0; i < n; i++) {
+      history.pushState(i, "num" + i, "?h-" + i)
+    }
+
+    a.remove()
+    render()
   }
+
+  document.body.appendChild(a);
+
 }
 
-const r = (length, s) => Array.from({length}, _ => s).join('') 
+const r = (length, s) => Array.from({ length }, _ => s).join('')
+
+const el = document.createElement('div')
+document.body.appendChild(el);
 
 const render = () => {
   const i = window.history.state
-  document.body.innerText = r(i, '.') + '|' + r(n - i - 1, '.')
+  el.innerText = r(i, '.') + '|' + r(n - i - 1, '.')
 
-  document.body.style.transform = 
-    `translateX(${((i/n) - 0.5) * 20}%)`
+  document.body.style.transform =
+    `translateX(${((i / n) - 0.5) * 20}%)`
 }
 
 render()
+
 
 window.addEventListener('popstate', render)
