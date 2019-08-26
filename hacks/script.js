ab8e3266fdc5fbcf9d14db51a7a81473beda2a72
Ben Foxall <benfoxall@gmail.com>
2019-08-26 23:37:32 +0100

    Positioning & DOM binding

--- hacks/script.js
+++ hacks/script.js
@@ -1,7 +1,41 @@
 const n = 13;
 
+const dom = (parent, array, cb) => {
+  if(parent.children.length !== array.length) {
+    parent.innerHTML = ''
+    array.forEach((value, i) => {
+      const el = document.createElement('div')
+      parent.appendChild(el)
+      cb(value, el, i);
+    }) 
+  } else {
+    array.forEach((value, i) => {
+      cb(value, parent.children[i], i);
+    }) 
+  }
+}
+
+const base  =  document.createElement('div')
+document.body.appendChild(base);
+
+const bulletsBase  =  document.createElement('div')
+document.body.appendChild(bulletsBase);
+
+
 const r = (length, s) => Array.from({ length }, _ => s).join('')
 const text = (i) => r(i, '.') + '△' + r(n - i - 1, '.')
+
+
+dom(base, text(5).split(''), (value, el, i) => {
+  el.innerText = value
+  Object.assign(
+    el.style, {
+      position: 'absolute',
+      left: '50%',
+      transform: `translate(-50%, 0) translate(${(i - n/2) * 5}vw, 0)`
+    }
+  )
+})
 
 const el = document.createElement('div')
 el.className = 'base'
@@ -9,7 +43,20 @@ document.body.appendChild(el);
 
 const render = () => {
   const i = window.history.state
-  el.innerText = text(i)
+  // el.innerText = text(i)
+
+  dom(base, text(i).split(''), (value, el, i) => {
+    el.innerText = value
+    Object.assign(
+      el.style, {
+        position: 'absolute',
+        left: '50%',
+        bottom: '0',
+        transform: `translate(-50%, 0) translate(${(i - n/2) * 5}vw, 0)`
+      }
+    )
+  })
+
 }
 
 window.addEventListener('popstate', render)
@@ -52,21 +99,31 @@ if (!window.history.state) {
   const loop = () => {
     const now = + new Date;
 
-    for(const el of document.querySelectorAll('.bullet')) {
-      el.remove()
-    }
+
+    dom(bulletsBase, bullets, (value, el) => {
+      // debugger;
+      const [i, b] = value
+
+      el.className = 'bullet'
+      el.textContent = '▲'
+      el.style.transform = `translate(-50%, ${(b - now) / 25}vh) translateX(${(i - n/2) * 5}vw)`
+    })
+
+    // for(const el of document.querySelectorAll('.bullet')) {
+    //   el.remove()
+    // }
 
 
-    bullets
-      .forEach(([i, b]) => {
+    // bullets
+    //   .forEach(([i, b]) => {
 
-        const x = document.createElement('div')
-        x.className = 'bullet'
-        x.textContent = '▲'
-        x.style.transform = `translate(-50%, ${(b - now) / 25}vh) translateX(${(i - n/2) * 5}vw)`
-        document.body.appendChild(x);
+    //     const x = document.createElement('div')
+    //     x.className = 'bullet'
+    //     x.textContent = '▲'
+    //     x.style.transform = `translate(-50%, ${(b - now) / 25}vh) translateX(${(i - n/2) * 5}vw)`
+    //     document.body.appendChild(x);
 
-      })
+    //   })
 
     bullets
       = bullets.filter(([_, b]) => 
